d9d826265632c225a9696866a6d5355b1bb774fc
daniels1101 <danher559@fusdk12.net>
2022-05-16 19:10:32 -0700

    Select Players, Select Colors, Select Names

--- Alan AI.js
+++ Alan AI.js
@@ -12,12 +12,16 @@ var alanBtnInstance = alanBtn({
 		setup()
 		}
 	},
-    onCommand: function (commandData){
-        if (commandData.command === 'Alanplayernumber'){
-            document.getElementById('playernumber').value = 2
-            playernumber_onchange()
-          }
-      },
+  onCommand: function (commandData){
+      if (commandData.command.includes('Alanplayernumber') ){
+        console.log(commandData.command.replace('Alanplayernumber','')) 
+        document.getElementById('playernumber').value = commandData.command.replace('Alanplayernumber','')
+          playernumber_onchange()
+       }
+      ChangeColor(commandData.command)
+      ChangePlayerName(commandData.command, commandData.payload)
+        },
+    
     
     rootEl: document.getElementById("alan-btn"),
   });
@@ -43,3 +47,23 @@ var alanBtnInstance = alanBtn({
 
   //this is how you can turn off the Alan Button programmatically
   alanBtnInstance.deactivate();
+
+  function ChangeColor(cmd){
+  if (cmd.includes('AlanPlayer') && cmd.includes('Color')){
+    var Colorvar = cmd.split('Color')
+    document.getElementById('player'+Colorvar[0].replace('AlanPlayer','') +'color').value = Colorvar[1][0].toUpperCase()+Colorvar[1].substring(1)
+        }
+      }
+
+ function ChangePlayerName(cmd, pyd){
+  if (cmd === 'AlanPlayerName'){
+    document.getElementById('player'+ pyd.PlayerNumber +'name').value = pyd.PlayerName[0].toUpperCase()+pyd.PlayerName.substring(1)
+        }
+ }
+ 
+ /*function ChangePlayerName(cmd){
+  if (cmd.includes('AlanPlayerN') && cmd.includes('Name')){
+    var Namevar = cmd.split('Name')
+    document.getElementById('player'+Namevar[0].replace('AlanPlayerN','') +'name').value = Namevar[1][0].toUpperCase()+Namevar[1].substring(1)
+        }
+ }*/
